457bb84dc381f595f1345c5d6af1e9d2076790b5
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-11-25 12:26:26 +0800

    Added fetching latest earthquake data from USGS url in AsyncTask background thread

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -11,11 +11,23 @@ public class Earthquake {
     // Time of the earthquake
     private long mTimeInMilliseconds;
 
-    // Construct a new Earthquake object
-    public Earthquake(double magnitude, String location, long timeInMilliseconds) {
+    // Website URL of the earthquake
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
+     */
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         mMagnitude = magnitude;
         mLocation = location;
         mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
 
     // Returns the magnitude of the earthquake
@@ -27,5 +39,10 @@ public class Earthquake {
     // Returns the time of the earthquake
     public long getTimeInMilliseconds() {
         return mTimeInMilliseconds;
+    }
+
+    // Returns the website URL to find more information about the earthquake.
+    public String getUrl() {
+        return mUrl;
     }
 }
