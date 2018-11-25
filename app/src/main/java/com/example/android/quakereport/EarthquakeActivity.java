e86378e008a566abfe70c937cb9ab6cc6042f0a3
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-11-25 13:46:59 +0800

    Added circular progress bar indicator

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -115,6 +115,10 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
         Log.i(LOG_TAG, "TEST: onLoadFinished() called...");
 
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
         // Set empty state text to display "No earthquakes found."
         mEmptyStateTextView.setText(R.string.no_earthquakes);
 
